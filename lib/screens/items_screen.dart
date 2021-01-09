c12279da090de81169984d08a854ec63e942e9b4
Bouff <a.boufroum@gmail.com>
2021-01-09 14:58:13 +0100

    deleted the itemAssetNumber list

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -3,13 +3,6 @@ import 'package:flutter/material.dart';
 
 class ItemsScreen extends StatelessWidget {
   final List data; // le JSON est sous forme de liste et non une Map
-  final List<String> itemsAssetnumber = [
-    "01", "02", "03", "04", "05", "06", "07", "08", "09",
-    "11", "12", "13", "14", "15", "16", "17", "18", "19",
-     "22", "23", "24", "25", "26", "27", "28", "29", "33",
-      "34", "35", "36", "37", "38", "39", "44", "45", "46",
-       "47", "48", "49", "55", "57", "58", "59", "66", "67",
-        "68", "69", "77", "78", "79", "88", "89", "99"]; // une Liste avec les nom des item dans Assets
   ItemsScreen({Key key, this.data}) : super(key: key);
   
   @override
@@ -26,7 +19,8 @@ class ItemsScreen extends StatelessWidget {
               title: Text(data[index]['name']),
               subtitle: Text(data[index]['description']),
               leading: CircleAvatar(
-                backgroundImage: AssetImage('assets/items/${itemsAssetnumber[index]}.png')),
+                backgroundImage: AssetImage('assets/items/${data[index]['id'].toString()}.png')),
+                // need an other Cardtype to show the items effects
              
           ));
         },
