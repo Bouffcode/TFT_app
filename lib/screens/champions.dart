7f24b83a770947b394c39ccb8f1efae600f23115
Bouff <a.boufroum@gmail.com>
2021-01-10 15:48:48 +0100

    championsWithListViewBuilder

--- lib/screens/champions.dart
+++ lib/screens/champions.dart
@@ -7,25 +7,17 @@ class ChampionsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: <Widget>[
-          Column(
-            children: [
-              new Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration:  BoxDecoration(
-                      shape: BoxShape.circle,
-                      image:  DecorationImage(
-                          fit: BoxFit.fill,
-                          image:  AssetImage(
-                              "assets/champions/${data[0]['apiName']}.png")))),
-                              Text(data[0]['name'], textScaleFactor: 1.5),
-                              Text(data[0]['ability']['desc']),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+      body:ListView.builder(
+        itemCount: data.length, //TO DO length
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              onTap: (){},
+              title: Text(data[index]['name']),
+              subtitle: Text(data[index]['ability']['desc']),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/champions/${data[index]['apiName']}.png')),
+                // need an other Cardtype to show the items effects
+             
+          ));}
+      ),);}}
