add343e7c2128d2181975b11b362dad5846ee5eb
Bouff <a.boufroum@gmail.com>
2021-01-10 15:01:48 +0100

    Corrected championsJson

--- lib/screens/champions.dart
+++ lib/screens/champions.dart
@@ -4,8 +4,25 @@ class ChampionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          Row(children: <Widget>[
+            Column(
+              children: [
+                new Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration:  BoxDecoration(
+                        shape: BoxShape.circle,
+                        image:  DecorationImage(
+                            fit: BoxFit.fill,
+                            image:  AssetImage(
+                                "assets/champions/${data[0]['apiName']}.png")))),
+                                Text(data[0]['name'], textScaleFactor: 1.5),
+              ],
+            ),
+
       ],),
     );
   }
